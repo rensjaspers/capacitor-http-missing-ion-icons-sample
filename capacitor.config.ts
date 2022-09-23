b0a3101c7d263dc9343d29094cae0064e3a76a49
Rens Jaspers <rens.jaspers@gmail.com>
2022-09-23 11:20:26 +0200

    Add ios with CapacitorHttp enabled

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,19 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'capacitor-http-missing-ion-icons-sample',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    CapacitorCookies: {
+      enabled: true,
+    },
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
