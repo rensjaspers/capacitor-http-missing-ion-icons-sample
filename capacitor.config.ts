55f6512d8c33cc97699e7fd854d6381441a90655
Rens Jaspers <rens.jaspers@gmail.com>
2022-09-23 11:11:29 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-http-missing-ion-icons-sample',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
